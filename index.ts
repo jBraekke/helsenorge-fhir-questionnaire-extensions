53281f5f8dab6ebcfe706eda3577b2dab0205d57
Jørgen H. Brække <jorgen.brakke@capgemini.com>
2021-02-11 14:39:56 +0100

    resources added

--- index.ts
+++ index.ts
@@ -1,9 +1,87 @@
-import entryFormat from './resources/structureDefinition/entryformat-extension';
-import maxValue from './resources/structureDefinition/maxvalue-extension';
+import entryFormat from './resources/structureDefinition/entryformat-extension.json';
+import itemControl from './resources/structureDefinition/itemcontrol-extension.json';
+import maxDecimalplaces from './resources/structureDefinition/maxdecimalplaces-extension.json';
+import maxValue from './resources/structureDefinition/maxvalue-extension.json';
+import minValue from './resources/structureDefinition/minvalue-extension.json';
+import minLength from './resources/structureDefinition/minlength-extension.json';
+import questionnaireHidden from './resources/structureDefinition/questionnaire-hidden.json';
+import questionnaireOrdinalValue from './resources/structureDefinition/questionnaire-ordinalvalue.json';
+import regexExtension from './resources/structureDefinition/regex-extension.json';
+import accessibilityToResponseExtension from './resources/structureDefinition/sdf-accessibilitytoresponse-extension.json';
+import authenticationRequirementExtension from './resources/structureDefinition/sdf-AuthenticationRequirement-extension.json';
+import canBePerformedByExtension from './resources/structureDefinition/sdf-CanBePerformedBy-extension.json';
+import discretionExtension from './resources/structureDefinition/sdf-discretion-extension.json';
+import endpointExtension from './resources/structureDefinition/sdf-endpoint-extension.json';
+import fhirPathExtension from './resources/structureDefinition/sdf-fhirpath-extension.json';
+import optionReferenceExtension from './resources/structureDefinition/sdf-optionReference-extension.json';
+import organizationProfile from './resources/structureDefinition/sdf-organization-profile.json';
+import personProfile from './resources/structureDefinition/sdf-person-profile.json';
+import questionnaireNavgiatorState from './resources/structureDefinition/sdf-questionnaire-navgiator-state.json';
+import questionnairePrintVersion from './resources/structureDefinition/sdf-questionnaire-print-version.json';
+import questionnaireProfile from './resources/structureDefinition/sdf-questionnaire-profile.json';
+import questionnaireresponseProfile from './resources/structureDefinition/sdf-questionnaireresponse-profile.json';
+import repeatstextExtension from './resources/structureDefinition/sdf-repeatstext-extension.json';
+import saveToDocumentArchiveExtension from './resources/structureDefinition/sdf-savetodocumentarchive-extension.json';
+import validationTextExtension from './resources/structureDefinition/sdf-validationtext-extension.json';
+import shareableCodeSystemProfile from './resources/structureDefinition/shareablecodesystem-profile.json';
+import shareableValuesetProfile from './resources/structureDefinition/shareablevalueset-profile.json';
 
 export {
     entryFormat,
-    maxValue
+    itemControl,
+    maxDecimalplaces,
+    maxValue,
+    minValue,
+    minLength,
+    questionnaireHidden,
+    questionnaireOrdinalValue,
+    regexExtension,
+    accessibilityToResponseExtension,
+    authenticationRequirementExtension,
+    canBePerformedByExtension,
+    discretionExtension,
+    endpointExtension,
+    fhirPathExtension,
+    optionReferenceExtension,
+    organizationProfile,
+    personProfile,
+    questionnaireNavgiatorState,
+    questionnairePrintVersion,
+    questionnaireProfile,
+    questionnaireresponseProfile,
+    repeatstextExtension,
+    saveToDocumentArchiveExtension,
+    validationTextExtension,
+    shareableCodeSystemProfile,
+    shareableValuesetProfile,
 };
 
-export default [entryFormat, maxValue];
+export default [
+    entryFormat,
+    itemControl,
+    maxDecimalplaces,
+    maxValue,
+    minValue,
+    minLength,
+    questionnaireHidden,
+    questionnaireOrdinalValue,
+    regexExtension,
+    accessibilityToResponseExtension,
+    authenticationRequirementExtension,
+    canBePerformedByExtension,
+    discretionExtension,
+    endpointExtension,
+    fhirPathExtension,
+    optionReferenceExtension,
+    organizationProfile,
+    personProfile,
+    questionnaireNavgiatorState,
+    questionnairePrintVersion,
+    questionnaireProfile,
+    questionnaireresponseProfile,
+    repeatstextExtension,
+    saveToDocumentArchiveExtension,
+    validationTextExtension,
+    shareableCodeSystemProfile,
+    shareableValuesetProfile,
+];
